7bffba80051bc0aaf1a7e51171b65087fc01d943
R-Rijnbeek <robert270384@gmail.com>
2023-11-10 17:58:05 +0100

    inicial dataframe structure

---
+++ app/public/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template
+
+from app.shared import LOG
+from app.utils import MuSuperMethod
+
+from . import test
+
+@test.route("/test", methods=["GET"])
+def get_userlist():
+    LOG.info("HolAHOLA")
+    MuSuperMethod()
+    return render_template("public/html/test.html"), 200			
+
+# =============== EXECUTE TEST CODE ===============
+
+if __name__ == "__main__":
+    pass
