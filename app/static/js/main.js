c9118978ead9c84e1f013405c952642d95682da5
R-Rijnbeek <robert270384@gmail.com>
2023-11-16 15:39:42 +0100

    finished the main functionality

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,6 @@
 
 window.addEventListener('load', function() {
+
     var temperature_slider = document.getElementById("temperature");
 
     var temperature_value = document.getElementById("temperature_value");
@@ -20,10 +21,12 @@ window.addEventListener('load', function() {
     relative_humidity_slider.oninput = function() {
         relative_humidity_value.innerHTML = this.value;
     }
+
+    AirPropertiesCalculation();
     
 })
 
-function ajaxWithRefresh(){
+function AirPropertiesCalculation(){
     let temp = $('#temperature').val()
     let hr = $('#relative_humidity').val();
     $.ajax(
@@ -35,15 +38,55 @@ function ajaxWithRefresh(){
             "hr": hr
         },
         success: function(response){
-            console.log("user succesfull stored in database: " + JSON.stringify(response))
-            //$('#name').val('')
-            //$('#email').val('')
-            //location.reload()
+            console.log("INFO: Succesfull calculation" + JSON.stringify(response))
+            refrehTable(response)
         },
         error: function(error){
-            alert("Error storing information in database: error code: " + error.status)
-            //$('#name').val('')
-            //$('#email').val('')
+            console.log("ERROR: Unespected error => " + error.status)
         },
     });
+}
+
+function refrehTable(response) {
+    
+    var table = document.createElement('TABLE');
+    table.classList.add("w3-table");
+    table.classList.add("w3-striped");
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    var header = document.createElement('TR');
+    tableBody.appendChild(header);
+ 
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("properties"));
+    header.appendChild(th);
+
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("value"));
+    header.appendChild(th);
+
+
+    tableBody.appendChild(header);
+
+    air_info = response.processed
+        
+    Object.entries(air_info).forEach(([key, value]) => {
+
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(key));
+        tr.appendChild(td);
+
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(value));
+        tr.appendChild(td);
+  
+    });
+    
+    var myTableDiv = document.getElementById("tableDiv");
+    myTableDiv.innerHTML = table.outerHTML;
 }
