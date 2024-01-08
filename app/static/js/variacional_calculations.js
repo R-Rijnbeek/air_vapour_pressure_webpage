a15fd8a1d785cca23abdd877fa1e9c0267322468
R-Rijnbeek <robert270384@gmail.com>
2024-01-08 23:47:06 +0100

    firts try activating post request on frontend

--- app/static/js/variacional_calculations.js
+++ app/static/js/variacional_calculations.js
@@ -28,4 +28,29 @@ window.addEventListener('load', function() {
 
 function AirPropertiesChangeCalculation(){
     alert("hola")
+    let temp = $('#temperature').val()
+    let delta_temp = $('#temperature_change').val() 
+    let hr = $('#relative_humidity').val();
+    let delta_hr = $('#relative_humidity_change').val()
+    $.ajax(
+        {
+        url:"/post_variacional_request",
+        type:"POST",
+        data: {
+            "temp": temp,
+            "delta_temp": delta_temp,
+            "hr": hr,
+            "delta_hr": delta_hr
+        },
+        success: function(response){
+            console.log("INFO: Succesfull calculation" + JSON.stringify(response))
+            //refrehTable(response)
+            alert("buenas")
+        },
+        error: function(error){
+            console.log("ERROR: Unespected error => " + error.status)
+        },
+    });
 }
+
+
