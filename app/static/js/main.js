495499d2f4b2e73c3ab34d70cd6840bf79e13f1f
R-Rijnbeek <robert270384@gmail.com>
2024-01-09 11:15:19 +0100

    Put refresh Table method on main js module

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -13,3 +13,46 @@ function AddEventListenerOnSlider(sliderDOM, method) {
     })
 }
 
+function refrehTable(response) {
+    
+    var table = document.createElement('TABLE');
+    table.classList.add("w3-table");
+    table.classList.add("w3-striped");
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    var header = document.createElement('TR');
+    tableBody.appendChild(header);
+ 
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("properties"));
+    header.appendChild(th);
+
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("value"));
+    header.appendChild(th);
+
+
+    tableBody.appendChild(header);
+
+    air_info = response.processed
+        
+    Object.entries(air_info).forEach(([key, value]) => {
+
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(key));
+        tr.appendChild(td);
+
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(value));
+        tr.appendChild(td);
+  
+    });
+    
+    var myTableDiv = document.getElementById("tableDiv");
+    myTableDiv.innerHTML = table.outerHTML;
+}
