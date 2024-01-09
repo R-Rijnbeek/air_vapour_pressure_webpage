7e20053df58d10e8534eb57322167a2a65871a0d
R-Rijnbeek <robert270384@gmail.com>
2024-01-09 11:10:17 +0100

    first working version

--- app/static/js/variacional_calculations.js
+++ app/static/js/variacional_calculations.js
@@ -43,12 +43,56 @@ function AirPropertiesChangeCalculation(){
         },
         success: function(response){
             console.log("INFO: Succesfull calculation" + JSON.stringify(response))
-            //refrehTable(response)
+            refrehTable(response)
         },
         error: function(error){
             console.log("ERROR: Unespected error => " + error.status)
         },
     });
+}
+
+function refrehTable(response) {
+    
+    var table = document.createElement('TABLE');
+    table.classList.add("w3-table");
+    table.classList.add("w3-striped");
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+    var header = document.createElement('TR');
+    tableBody.appendChild(header);
+ 
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("properties"));
+    header.appendChild(th);
+
+    var th = document.createElement('TH');
+    th.appendChild(document.createTextNode("value"));
+    header.appendChild(th);
+
+
+    tableBody.appendChild(header);
+
+    air_info = response.processed
+        
+    Object.entries(air_info).forEach(([key, value]) => {
+
+        var tr = document.createElement('TR');
+        tableBody.appendChild(tr);
+
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(key));
+        tr.appendChild(td);
+
+        var td = document.createElement('TD');
+        td.appendChild(document.createTextNode(value));
+        tr.appendChild(td);
+  
+    });
+    
+    var myTableDiv = document.getElementById("tableDiv");
+    myTableDiv.innerHTML = table.outerHTML;
 }
 
 
