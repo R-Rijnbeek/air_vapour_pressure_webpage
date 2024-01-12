6054117a8fee8139d20644baf7fd0201c047504a
R-Rijnbeek <robert270384@gmail.com>
2024-01-12 13:12:42 +0100

    Third Tab created

--- app/public/__init__.py
+++ app/public/__init__.py
@@ -4,8 +4,10 @@ from flask import Blueprint
 
 # =============== DEFINE BLUEPRINTS ==============
 
-test = Blueprint('test', __name__, template_folder = 'templates')
+tab1 = Blueprint('tab1', __name__, template_folder = 'templates')
 
-api = Blueprint('api', __name__)
+tab2 = Blueprint('tab2', __name__, template_folder = 'templates')
+
+tab3 = Blueprint('tab3', __name__, template_folder = 'templates')
 
 from . import routes
