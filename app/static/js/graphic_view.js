24c2524605cd65d13046d089519ef16b0b309bf8
R-Rijnbeek <robert270384@gmail.com>
2024-01-15 13:23:58 +0100

    restructure js for better datamanagement

--- app/static/js/graphic_view.js
+++ app/static/js/graphic_view.js
@@ -1,7 +1,7 @@
 var globalVars = {unloaded:true};
 var series1;
-var series2;
-var xAxis;
+//var xAxis;
+
 $(window).bind('beforeunload', function(){
     console.log("before unload");
     globalVars.unloaded = true;
@@ -9,106 +9,90 @@ $(window).bind('beforeunload', function(){
 
 function reloadData() {
     var newdata = [{ 
-        category: "Research", 
-        value1: 2000, 
-        value2: 588 
-      }, { 
-        category: "Marketing", 
-        value1: 1240, 
-        value2: 1800 
-      }, { 
-        category: "Sales", 
-        value1: 850, 
-        value2: 1430 
-      }];
+            "RH": 1, 
+            "ab_hu": 1200
+        }, { 
+            "RH": 2, 
+            "ab_hu": 2000
+        }, { 
+            "RH": 3, 
+            "ab_hu": 880
+        }];
     return newdata;
 }
 
 function loadNewData() {
-    // var chartData = reloadData();
     series1.data.setAll(reloadData());
-    series2.data.setAll(reloadData());
-    xAxis.data.setAll(reloadData());
-    // chart.validateData();
+    //xAxis.data.setAll(reloadData());
 }
 
 
 window.addEventListener('load', function() {
-    // Create root and chart
-var root = am5.Root.new("chartdiv"); 
-var chart = root.container.children.push( 
-  am5xy.XYChart.new(root, {
-    panY: false,
-    layout: root.verticalLayout
-  }) 
-);
 
-// Define data
-var data = [{ 
-  category: "Research", 
-  value1: 1000, 
-  value2: 588 
-}, { 
-  category: "Marketing", 
-  value1: 2200, 
-  value2: 1800 
-}, { 
-  category: "Sales", 
-  value1: 850, 
-  value2: 1230 
-}];
+    var data = [{ 
+        "RH": 1, 
+        "ab_hu": 1000
+        }, { 
+        "RH": 2, 
+        "ab_hu": 2200
+        }, { 
+        "RH": 3, 
+        "ab_hu": 850
+    }];
 
-// Craete Y-axis
-var yAxis = chart.yAxes.push( 
-  am5xy.ValueAxis.new(root, { 
-    min: 0,
-    max: 3000,
-    renderer: am5xy.AxisRendererY.new(root, {}) 
-  }) 
-);
-
-// Create X-Axis
-xAxis = chart.xAxes.push(
-  am5xy.CategoryAxis.new(root, {
-    renderer: am5xy.AxisRendererX.new(root, {}),
-    categoryField: "category"
-  })
-);
-xAxis.data.setAll(data);
-
-// Create series
-series1 = chart.series.push( 
-  am5xy.ColumnSeries.new(root, { 
-    name: "Series", 
-    xAxis: xAxis, 
-    yAxis: yAxis, 
-    valueYField: "value1", 
-    categoryXField: "category" 
-  }) 
-);
-series1.data.setAll(data);
-
-series2 = chart.series.push( 
-  am5xy.ColumnSeries.new(root, { 
-    name: "Series", 
-    xAxis: xAxis, 
-    yAxis: yAxis, 
-    valueYField: "value2", 
-    categoryXField: "category" 
-  }) 
-);
-series2.data.setAll(data);
-
-// Add legend
-var legend = chart.children.push(am5.Legend.new(root, {})); 
-legend.data.setAll(chart.series.values);
-
-// Add cursor
-chart.set("cursor", am5xy.XYCursor.new(root, {}));
-
-
-
-
+    loadAmChart(data)
 })
 
+function loadAmChart(data) {
+
+    // Create root and chart
+    var root = am5.Root.new("chartdiv"); 
+    var chart = root.container.children.push( 
+        am5xy.XYChart.new(root, {
+            panY: false,
+            layout: root.verticalLayout
+        }) 
+    );
+
+    // Craete Y-axis
+    var yAxis = chart.yAxes.push( 
+        am5xy.ValueAxis.new(root, { 
+            min: 0,
+            max: 3000,
+            renderer: am5xy.AxisRendererY.new(root, {}) 
+        }) 
+    );
+
+    // Create X-Axis
+    xAxis = chart.xAxes.push(
+        am5xy.CategoryAxis.new(root, {
+            renderer: am5xy.AxisRendererX.new(root, {}),
+            min: 0,
+            max: 100,
+            categoryField: "RH"
+        })
+    );
+    xAxis.data.setAll(data);
+
+    // Create series
+    series1 = chart.series.push( 
+        am5xy.LineSeries.new(root, { 
+            name: "Series", 
+            xAxis: xAxis, 
+            yAxis: yAxis, 
+            valueYField: "ab_hu", 
+            categoryXField: "RH" 
+        }) 
+    );
+    series1.data.setAll(data);
+
+    // Add legend
+    var legend = chart.children.push(
+        am5.Legend.new(root, {}
+    )); 
+    legend.data.setAll(chart.series.values);
+
+    // Add cursor
+    chart.set("cursor", am5xy.XYCursor.new(root, {}));
+}
 
