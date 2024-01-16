90953cb8489f3cc9887ef77542fdceb8181c4304
R-Rijnbeek <robert270384@gmail.com>
2024-01-16 15:17:14 +0100

    funished putting filters on footer

--- app/static/js/data_table.js
+++ app/static/js/data_table.js
@@ -21,14 +21,63 @@ function createDataTableRequest() {
     });
 }
 
+
+function addingFooterToTable(response) {
     
+    collums = response.columns
+    
+    table = document.getElementById("DataTable")//.innerHTML = "<tfoot><tr><th>Names</th><th>Position</th><th>Office</th><th>Age</th><th>Start date</th</tr></tfoot>"
+    
+    var foot = document.createElement('tfoot');
+    table.appendChild(foot);
 
+    var header = document.createElement('TR');
+    foot.appendChild(header);
 
-function CreateDataTableByResponse(response) {
-    const table = new DataTable('#DataTable', {
-        data: response.data,
-        columns: response.columns 
+    collums.forEach((element) => {
+        var th = document.createElement('TH');
+        th.appendChild(document.createTextNode(element.title));
+        header.appendChild(th);
     })
 
 }
+
+
+
+function CreateDataTableByResponse(response) {
+
+    addingFooterToTable(response)
+
+    new DataTable('#DataTable', {
+        data: response.data,
+        columns: response.columns,
+        initComplete: function () {
+            this.api()
+                .columns()
+                .every(function () {
+                    let column = this;
+                    let title = column.footer().textContent;
+     
+                    // Create input element
+                    let input = document.createElement('input');
+                    input.placeholder = title;
+                    column.footer().replaceChildren(input);
+     
+                    // Event listener for user input
+                    input.addEventListener('keyup', () => {
+                        if (column.search() !== this.value) {
+                            column.search(input.value).draw();
+                        }
+                    });
+                });
+        }
+        
+    })
+
+    
+} 
+
+    
+
+
     
